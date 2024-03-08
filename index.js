5d223a293b5935f43ceb497a549c60f780d9c60d
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:20:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+9.; // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+const favorite_number = 5289;
+console.log(`my favourite number is ${favorite_number}`);
---
+++ index.ts
@@ -0,0 +1,6 @@
+9.// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+const favorite_number: number = 5289
+
+console.log (`my favourite number is ${favorite_number}`);
+
